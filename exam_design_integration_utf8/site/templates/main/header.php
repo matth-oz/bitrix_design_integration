ead23d5e9930597c1ab7b29e1c83ececcd993cab
matth_oz <matth_oz@mail.ru>
2018-03-31 15:59:47 +0300

    header.php edited: including of css and js files fixed

--- exam_design_integration_utf8/site/templates/main/header.php
+++ exam_design_integration_utf8/site/templates/main/header.php
@@ -8,11 +8,13 @@ IncludeTemplateLangFile(__FILE__);
 	<meta charset="<?=LANG_CHARSET?>">
 	<?$APPLICATION->ShowHead();?>
 	<title><?$APPLICATION->ShowTitle()?></title>
-	<link rel="stylesheet" href="/bitrix/templates/.default/template_styles.css"/>
-	<script type="text/javascript" src="/bitrix/templates/.default/js/jquery-1.8.2.min.js"></script>
-	<script type="text/javascript" src="/bitrix/templates/.default/js/slides.min.jquery.js"></script>
-	<script type="text/javascript" src="/bitrix/templates/.default/js/jquery.carouFredSel-6.1.0-packed.js"></script>
-	<script type="text/javascript" src="/bitrix/templates/.default/js/functions.js"></script>
+	<?
+	$APPLICATION->SetAdditionalCSS('/bitrix/templates/.default/template_styles.css');
+	$APPLICATION->AddHeadScript('/bitrix/templates/.default/js/jquery-1.8.2.min.js');
+	$APPLICATION->AddHeadScript('/bitrix/templates/.default/js/slides.min.jquery.js');
+	$APPLICATION->AddHeadScript('/bitrix/templates/.default/js/jquery.carouFredSel-6.1.0-packed.js');
+	$APPLICATION->AddHeadScript('/bitrix/templates/.default/js/functions.js');	
+	?>
 	
 	<!--[if gte IE 9]><style type="text/css">.gradient {filter: none;}</style><![endif]-->
 </head>
